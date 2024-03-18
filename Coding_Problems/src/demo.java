88ff17ca02122379b003679c8640b42e251fdc29
prathamesh nale <prathameshn7077@gmail.com>
2024-03-18 23:52:05 +0530

    adding interview questions/answers

--- Coding_Problems/src/demo.java
+++ Coding_Problems/src/demo.java
@@ -5,16 +5,6 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner sc = new Scanner(System.in);
-		String str1="prathamesh";
-		
-		for(int i=65;i<=90;i++) {
-			for(int j=0;j<str1.length();j++) {
-				if(i==str1.charAt(j)) {
-					
-				}
-			}
-		}
 		
 	}
 
